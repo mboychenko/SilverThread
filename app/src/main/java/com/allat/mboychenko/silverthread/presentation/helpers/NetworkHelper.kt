b8a2d2550b2e872b0e8cdccd00af6717793979e6
mboychenko <mikhail.boychenko@dataart.com>
2019-09-17 01:43:04 +0200

    Fix for Radio in API < LOLLIPOP code version 20 was not published, so will be 21 as target for update

--- app/src/main/java/com/allat/mboychenko/silverthread/presentation/helpers/NetworkHelper.kt
+++ app/src/main/java/com/allat/mboychenko/silverthread/presentation/helpers/NetworkHelper.kt
@@ -1,7 +1,6 @@
 package com.allat.mboychenko.silverthread.presentation.helpers
 
 import android.annotation.SuppressLint
-import android.annotation.TargetApi
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.net.ConnectivityManager
@@ -66,24 +65,80 @@ fun hasInternetAccess(context: Context, observer: (Boolean) -> Unit): Disposable
         .subscribe { result -> observer(result) }
 }
 
-@TargetApi(Build.VERSION_CODES.LOLLIPOP)
 class ConnectionStateMonitor(
     val context: Context,
     private val callerThread: Handler = Handler(Looper.getMainLooper()),
     val callback: InternetAvailability
-) : NetworkCallback() {
+) {
 
-    @Volatile private var hasInternet = false
+    @Volatile
+    private var hasInternet = false
 
     private val connectivityManager: ConnectivityManager =
         context.getSystemService(CONNECTIVITY_SERVICE) as ConnectivityManager
-    private val networkRequest: NetworkRequest =
-        NetworkRequest.Builder().addTransportType(NetworkCapabilities.TRANSPORT_CELLULAR)
-            .addTransportType(NetworkCapabilities.TRANSPORT_WIFI).build()
+
+    private lateinit var networkCallback: NetworkCallback
+    private lateinit var networkRequest: NetworkRequest
+
+    init {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            networkRequest =
+                NetworkRequest.Builder().addTransportType(NetworkCapabilities.TRANSPORT_CELLULAR)
+                    .addTransportType(NetworkCapabilities.TRANSPORT_WIFI).build()
+
+            networkCallback = object : NetworkCallback() {
+
+                /**
+                 * Calling on background ConnectivityThread
+                 */
+                override fun onAvailable(network: Network) {
+                    if (!hasInternet) {
+                        Handler().postDelayed(
+                            {
+                                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                                    val capabilities = connectivityManager.getNetworkCapabilities(network)
+                                    if (capabilities != null && capabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_VALIDATED)) {
+                                        hasInternet = true
+                                        executeOnHandlerThread(callerThread) { callback.internetAvailable() }
+                                    }
+                                } else {
+                                    val activeNetworkInfo = connectivityManager.activeNetworkInfo
+                                    if (activeNetworkInfo != null && activeNetworkInfo.isConnected && hasInternetAccess(
+                                            context
+                                        )
+                                    ) {
+                                        hasInternet = true
+                                        executeOnHandlerThread(callerThread) { callback.internetAvailable() }
+                                    }
+                                }
+                            },
+                            1500
+                        )        //delay for NET_CAPABILITY_VALIDATED, its need some time depend on network, usually 1.5 sec enough.
+                    }
+                }
+
+                /**
+                 * Calling on background ConnectivityThread
+                 */
+                override fun onLost(network: Network?) {
+                    hasInternet = false
+                    executeOnHandlerThread(callerThread) { callback.internetUnavailable() }
+                }
+
+                /**
+                 * Calling on background ConnectivityThread
+                 */
+                override fun onUnavailable() {
+                    hasInternet = false
+                    executeOnHandlerThread(callerThread) { callback.internetUnavailable() }
+                }
+            }
+        }
+    }
 
     fun enable() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            connectivityManager.registerNetworkCallback(networkRequest, this)
+            connectivityManager.registerNetworkCallback(networkRequest, networkCallback)
         } else {
             context.registerReceiver(internetStateReceiver, IntentFilter("android.net.conn.CONNECTIVITY_CHANGE"))
         }
@@ -92,52 +147,13 @@ class ConnectionStateMonitor(
     fun disable() {
         try {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-                connectivityManager.unregisterNetworkCallback(this)
+                connectivityManager.unregisterNetworkCallback(networkCallback)
             } else {
                 context.unregisterReceiver(internetStateReceiver)
             }
         } catch (e: IllegalArgumentException) {
             //if disable called before enable
         }
-    }
-
-    /**
-     * Calling on background ConnectivityThread
-     */
-    override fun onAvailable(network: Network) {
-        if (!hasInternet) {
-            Handler().postDelayed({
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                    val capabilities = connectivityManager.getNetworkCapabilities(network)
-                    if (capabilities != null && capabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_VALIDATED)) {
-                        hasInternet = true
-                        executeOnHandlerThread(callerThread) { callback.internetAvailable() }
-                    }
-                } else {
-                    val activeNetworkInfo = connectivityManager.activeNetworkInfo
-                    if (activeNetworkInfo != null && activeNetworkInfo.isConnected && hasInternetAccess(context)) {
-                        hasInternet = true
-                        executeOnHandlerThread(callerThread) { callback.internetAvailable() }
-                    }
-                }
-            }, 1500)        //delay for NET_CAPABILITY_VALIDATED, its need some time depend on network, usually 1.5 sec enough.
-        }
-    }
-
-    /**
-     * Calling on background ConnectivityThread
-     */
-    override fun onLost(network: Network?) {
-        hasInternet = false
-        executeOnHandlerThread(callerThread) { callback.internetUnavailable() }
-    }
-
-    /**
-     * Calling on background ConnectivityThread
-     */
-    override fun onUnavailable() {
-        hasInternet = false
-        executeOnHandlerThread(callerThread) { callback.internetUnavailable() }
     }
 
     private val internetStateReceiver = object : BroadcastReceiver() {
--- app/src/main/java/com/allat/mboychenko/silverthread/utils/UpdateAppFunctions.kt
+++ app/src/main/java/com/allat/mboychenko/silverthread/utils/UpdateAppFunctions.kt
@@ -22,17 +22,17 @@ fun updateVersion(context: Context) {
 }
 
 private fun updateScript(context: Context, storage: Storage) {
-    if (!storage.getBoolean(PATCH_20_APPLIED_PREF_KEY, false)) {
-        applyPatchVer20(context, storage)
-        storage.putBoolean(PATCH_20_APPLIED_PREF_KEY, true)
+    if (!storage.getBoolean(PATCH_21_APPLIED_PREF_KEY, false)) {
+        applyPatchVer21(context, storage)
+        storage.putBoolean(PATCH_21_APPLIED_PREF_KEY, true)
     }
 
-    applyPatchVer21()
+    applyPatchVer22()
 
     storage.putInt(LAST_UPDATE_VERSION_PREF, BuildConfig.VERSION_CODE)
 }
 
-private fun applyPatchVer20(context: Context, storage: Storage) {
+private fun applyPatchVer21(context: Context, storage: Storage) {
     removeAlarm(context, AlarmNotificationCodes.QUOTE.action, AlarmNotificationCodes.QUOTE.code)
     QuotesInteractor(storage).clearShowedTimesInDay()
 
@@ -48,10 +48,10 @@ private fun applyPatchVer20(context: Context, storage: Storage) {
     setupRandomQuoteNextAlarm(context, forceNextDay = true)
 }
 
-private fun applyPatchVer21() {
+private fun applyPatchVer22() {
     //future
 }
 
 private const val LAST_UPDATE_VERSION_PREF = "LAST_UPDATE_VERSION_PREF"
 
-private const val PATCH_20_APPLIED_PREF_KEY = "PATCH_20_APPLIED_PREF_KEY"
+private const val PATCH_21_APPLIED_PREF_KEY = "PATCH_21_APPLIED_PREF_KEY"
