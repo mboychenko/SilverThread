31fa2a6ccfcb2ee49bd7dbb6c0a29dd4286f477a
mboychenko <mikhail.boychenko@dataart.com>
2021-06-28 23:34:23 +0300

    - Added quotes & parables in Hungarian language - Share Practices option added version up to 5.4.2

--- app/src/main/java/com/allat/mboychenko/silverthread/presentation/views/dialogs/PracticeFormattedDialog.kt
+++ app/src/main/java/com/allat/mboychenko/silverthread/presentation/views/dialogs/PracticeFormattedDialog.kt
@@ -1,6 +1,8 @@
 package com.allat.mboychenko.silverthread.presentation.views.dialogs
 
+import android.content.Context
 import android.os.Bundle
+import android.text.Spannable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,23 +11,40 @@ import android.widget.TextView
 import androidx.fragment.app.DialogFragment
 import com.allat.mboychenko.silverthread.R
 import com.allat.mboychenko.silverthread.presentation.helpers.htmlFromAssetsCompat
+import com.allat.mboychenko.silverthread.presentation.views.fragments.IViewContext
 
-class PracticeFormattedDialog : DialogFragment() {
+class PracticeFormattedDialog : DialogFragment(), IViewContext {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         dialog?.window?.requestFeature(Window.FEATURE_NO_TITLE)
 
         val view = inflater.inflate(R.layout.prectice_dialog_fragment, container, false)
 
+        var practiceTitle: Spannable? = null
+        var practice: Spannable? = null
+
         arguments?.let {
-            it.getString(DIALOG_TITLE)?.let { title -> view.findViewById<TextView>(R.id.title).text = title }
+            it.getString(DIALOG_TITLE)?.let { title ->
+                view.findViewById<TextView>(R.id.title).text = title
+                practiceTitle = htmlFromAssetsCompat("<p style=\"text-align:center\"><b>$title</b></p>")
+            }
             it.getString(DIALOG_TEXT)?.let { text ->
-                view.findViewById<TextView>(R.id.text).text = htmlFromAssetsCompat(text)
+                practice = htmlFromAssetsCompat(text)
+                view.findViewById<TextView>(R.id.text).text = practice
             }
         }
 
+        val sharedText = StringBuilder().append(practiceTitle.toString()).append(practice.toString())
+
+        view.findViewById<View>(R.id.share).setOnClickListener {
+            shareText(sharedText.toString(), getString(R.string.share_practice))
+        }
         view.findViewById<View>(R.id.close).setOnClickListener { dismiss() }
         return view
+    }
+
+    override fun getViewContext(): Context? {
+        return context
     }
 
     override fun onResume() {
